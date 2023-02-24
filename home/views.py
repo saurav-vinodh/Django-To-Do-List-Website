cf3f4b550d59d357f32299a17b7c1043c3fa6185
saurav-vinodh <sauravvinodh2003@gmail.com>
2023-02-24 19:40:19 +0530

    First Commit

---
+++ home/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Task
+
+# Create your views here.
+
+def home(request):
+    context={'success':False}
+    if request.method == "POST":
+        title=request.POST['title']
+        desc=request.POST['desc']
+        print(title, desc)
+        ins=Task(taskTitle=title,taskDesc=desc)
+        ins.save()
+        context={ 'success': True}
+    return render(request, 'index.html', context)
+
+def tasks(request):
+    allTasks = Task.objects.all()
+    context={'tasks':allTasks}
+    print(allTasks)
+    return render(request, 'tasks.html',context)
+
+def delete(request, title):
+    obj=Task.objects.get(taskTitle=title)
+    obj.delete()
+    allTasks = Task.objects.all()
+    context={'tasks':allTasks}
+    print(allTasks)
+    return render(request, 'tasks.html', context)
+
+
